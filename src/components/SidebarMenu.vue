649c2cb423c55dbf9418630fa64773bffc2998b5
Morteza Jamshidi <devmor.j@gmail.com>
2022-06-12 03:16:56 +0430

    toggle sidebar

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -1,22 +1,29 @@
 <script setup lang="ts">
+import { shallowRef } from 'vue';
 import IconAngle from './icons/IconAngle.vue';
 import IconEmail from './icons/IconEmail.vue';
 import IconEmployeeGroup from './icons/IconEmployeeGroup.vue';
 import IconExclamation from './icons/IconExclamation.vue';
 import IconHome from './icons/IconHome.vue';
 
+const isSidebarOpen = shallowRef<Boolean>(false)
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+}
+
 </script>
 
 <template>
   <div class="wrapper">
-    <aside>
-      <ul>
+    <aside :vue:is-open="isSidebarOpen">
+      <ul class="sidebar-head">
         <li>
           <img src="@/assets/logo.png" alt="logo" width="32" height="32">
         </li>
         <li>
-          <button class="sidebar-toggle">
-            <IconAngle />
+          <button class="sidebar-toggle" :class="isSidebarOpen ? 'toggle-button' : ''">
+            <IconAngle @click="toggleSidebar" />
           </button>
         </li>
       </ul>
@@ -46,6 +53,9 @@ import IconHome from './icons/IconHome.vue';
 <style scoped lang="scss">
 @use '@/assets/scss/colors' as clr;
 
+$sidebar-width: 10rem;
+$toggle-duration: 300ms;
+
 aside {
   color: clr.$primary;
   background: clr.$bg-dark;
@@ -53,6 +63,12 @@ aside {
   flex-direction: column;
   min-height: 100vh;
   padding: 1rem;
+  transition: all $toggle-duration;
+  width: $sidebar-width;
+}
+
+aside[vue\:is-open=true] {
+  width: 2 * $sidebar-width;
 }
 
 ul > li {
@@ -61,7 +77,29 @@ ul > li {
   column-gap: 0.75rem;
 }
 
+img {
+  object-fit: contain;
+}
+
+.sidebar-head {
+  position: relative;
+  padding-block-end: 4rem;
+}
+
 button {
   cursor: pointer;
+  position: absolute;
+  transition-duration: $toggle-duration;
+  transition-property: transform, left, top;
+  left: 0;
+  top: 1rem;
+  transform: translateX(0%) translateY(2rem) rotateZ(0deg);
+
+  &.toggle-button {
+    color: red;
+    left: 100%;
+    top: 0;
+    transform: translateX(-100%) translateY(0rem) rotateZ(180deg);
+  }
 }
 </style>
