3dea78fc021c2a64b938eeda81b8822d2377a55f
Morteza Jamshidi <devmor.j@gmail.com>
2022-06-12 16:07:00 +0430

    build docs folder for github pages

---
+++ docs/assets/About.8c4d7ab9.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t}from"./index.0bab3a3b.js";const c={},r={class:"wrapper"};function n(s,a){return o(),t("div",r,"About Component")}var p=e(c,[["render",n]]);export{p as default};
